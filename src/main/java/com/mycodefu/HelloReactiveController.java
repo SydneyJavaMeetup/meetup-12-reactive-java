868f0a322477fc9e599b23784a15a6a0831bb4a9
Luke Thompson <endeavour9@gmail.com>
2023-01-29 12:14:30 +1100

    add reactive mongo driver code and test

--- src/main/java/com/mycodefu/HelloReactiveController.java
+++ src/main/java/com/mycodefu/HelloReactiveController.java
@@ -1,22 +1,34 @@
 package com.mycodefu;
 
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoCollection;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import jakarta.inject.Inject;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import static com.mongodb.client.model.Filters.eq;
 
 @Controller("/hello/reactive")
 public class HelloReactiveController {
 
+    @Inject
+    MongoClient mongoClient;
+
     @Get("/{name}")
     public Mono<Person> greet(String name) {
-        return Mono.just(new Person(name));
+        return Mono
+                .from(getPersonMongoCollection().find(eq("name", name)).first());
     }
 
     @Get(produces = MediaType.APPLICATION_JSON)
     public Flux<Person> findAll() {
-        return Flux.just(new Person("John"), new Person("Jane"));
+        return Flux.from(getPersonMongoCollection().find());
     }
 
+    private MongoCollection<Person> getPersonMongoCollection() {
+        return mongoClient.getDatabase("people").getCollection("person", Person.class);
+    }
 }
